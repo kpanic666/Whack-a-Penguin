61b2803be2871ad4c117ab17603c489a88246b48
Andrei Korikov <kpanic666@gmail.com>
2021-11-01 18:30:02 +0500

    game is done score is calculated you can hit penguins now

--- Whack-a-Penguin/GameScene.swift
+++ Whack-a-Penguin/GameScene.swift
@@ -16,6 +16,7 @@ class GameScene: SKScene {
     }
     var slots = [WhackSlot]()
     var popupTimer = 0.85
+    var numRounds = 0
 
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -42,7 +43,29 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            guard whackSlot.isVisible else { continue }
+            guard !whackSlot.isHit else { continue }
+            
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                score -= 5
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+                score += 1
+                
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
     }
     
     func createSlot(at position: CGPoint) {
@@ -53,6 +76,21 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
+        numRounds += 1
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            
+            return
+        }
+        
         popupTimer *= 0.991
         
         slots.shuffle()
